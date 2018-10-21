d9037930442c5a42edd9283cfbf4ebe904dcf48e
CHDQ <31378284+CHDQ@users.noreply.github.com>
2018-10-21 20:37:02 +0800

    增加数据类型demo

--- src/demo02_数据类型.go
+++ src/demo02_数据类型.go
@@ -1,6 +1,26 @@
 package main
 
+import "fmt"
 
 func main() {
-	
+	a := 100
+	var (
+		b byte = 100
+		c rune = 200
+		e byte = 'a' //uint8
+		f rune = 'a' //int32
+	)
+	fmt.Printf("%T , %v \n", a, a)
+	fmt.Printf("%T , %v \n", b, b)
+	fmt.Printf("%T , %v \n", c, c)
+	fmt.Printf("%T , %v \n", e, e)
+	fmt.Printf("%T , %v \n", f, f)
+	temp := `//原样输出字符串
+	x := 10
+	y := 20
+	fmt.Println(x, "\t", y)
+	x, y = y, x
+	fmt.Println(x, "\t", y)
+`
+	fmt.Println(temp)
 }
